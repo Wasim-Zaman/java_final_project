3b3b886fdf525eb176ac2bde21a260d4f3303fb5
Wasim Zaman <wasimxaman13@gmail.com>
2022-10-17 20:05:35 +0500

    desktop application added

---
+++ src/jdbc/assignment/pkg191035/Start.java
@@ -0,0 +1,11 @@
+package jdbc.assignment.pkg191035;
+
+public class Start {
+    public static void main(String[] args) {
+        
+        FirstFrame firstFrame = new FirstFrame();
+        firstFrame.setVisible(true);
+        firstFrame.setLocationRelativeTo(null);
+    }
+    
+}
